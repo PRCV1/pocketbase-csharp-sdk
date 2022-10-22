6a4191c0e2e4b5778510b3f7c54cb5012bae258c
LiangShuang <liangshuang@sutu.tech>
2022-10-22 17:07:04 +0800

    list/create/update items in base services

---
+++ pocketbase-csharp-sdk/Models/ItemBaseModel.cs
@@ -0,0 +1,23 @@
+﻿using pocketbase_csharp_sdk.Json;
+using System.Text.Json.Serialization;
+
+namespace pocketbase_csharp_sdk.Models
+{
+    public class ItemBaseModel
+    {
+        [JsonPropertyName("@collectionId")]
+        public string? CollectionId { get; set; }
+
+        [JsonPropertyName("@collectionName")]
+        public string? CollectionName { get; set; }
+
+        public string? Id { get; set; }
+
+        [JsonConverter(typeof(DateTimeConverter))]
+        public DateTime? Created { get; set; }
+
+        [JsonConverter(typeof(DateTimeConverter))]
+        public DateTime? Updated { get; set; }
+    }
+
+}
---
+++ pocketbase-csharp-sdk/Models/PagedCollectionModel.cs
@@ -0,0 +1,11 @@
+﻿namespace pocketbase_csharp_sdk.Models
+{
+    public class PagedCollectionModel<T>
+    {
+        public int Page { get; set; }
+        public int PerPage { get; set; }
+        public int TotalItems { get; set; }
+        public T[]? Items { get; set; }
+    }
+
+}
