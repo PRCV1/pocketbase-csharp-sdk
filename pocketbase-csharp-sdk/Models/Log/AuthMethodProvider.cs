f12481e8ca782964b3e98922589a6a8afea08642
PRCV1 <lukas.mueller.01@web.de>
2022-10-31 23:28:49 +0100

    moved authmodels

---
+++ pocketbase-csharp-sdk/Models/Log/AuthMethodProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pocketbase_csharp_sdk.Models.Log
+{
+    public class AuthMethodProvider
+    {
+        public string? Name { get; set; }
+        public string? State { get; set; }
+        public string? CodeVerifier { get; set; }
+        public string? CodeChallenge { get; set; }
+        public string? CodeChallengeMethod { get; set; }
+        public string? AuthUrl { get; set; }
+    }
+}
---
+++ pocketbase-csharp-sdk/Models/Log/AuthMethodsList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pocketbase_csharp_sdk.Models.Log
+{
+    public class AuthMethodsList
+    {
+        public bool? EmailPassword { get; set; }
+        public IEnumerable<AuthMethodProvider>? Provider { get; set; }
+    }
+}
