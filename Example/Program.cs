6ef0abfafb7c84b91319eac8550a0057d0ae37c8
iluvadev <iluvadev@gmail.com>
2023-01-04 13:25:57 +0100

    realtime draft

--- Example/Program.cs
+++ Example/Program.cs
@@ -4,11 +4,16 @@ using System.Diagnostics;
 
 Console.WriteLine("Hello, World!");
 
-PocketBase pocketBase = new PocketBase("http://127.0.0.1:8090");
-var adminUser = await pocketBase.Admin.AuthenticateWithPassword("test@test.de", "0123456789");
+PocketBase pocketBase = new PocketBase("https://orm-csharp-test.pockethost.io");//"http://127.0.0.1:8090");
+//var user2 = await pocketBase1.AuthCollection<Users2>("users2").AuthenticateWithPassword("iluvadev@gmail.com", "123456789");
 
-pocketBase.Records.Subscribe("restaurants", "*", cb =>
+//PocketBase pocketBase = new PocketBase("http://127.0.0.1:8090");
+//var adminUser = await pocketBase.Admin.AuthenticateWithPassword("test@test.de", "0123456789");
+
+//pocketBase.Records.Subscribe("restaurants", "*", cb =>
+pocketBase.Records.Subscribe("tags", "*", cb => 
 {
+    Console.WriteLine(cb.ToString());
     Debugger.Break();
 });
 
--- pocketbase-csharp-sdk/Services/BaseSubCrudService.cs
+++ pocketbase-csharp-sdk/Services/BaseSubCrudService.cs
@@ -1,6 +1,7 @@
 ﻿using pocketbase_csharp_sdk.Helper.Convert;
 using pocketbase_csharp_sdk.Models;
 using pocketbase_csharp_sdk.Models.Files;
+using pocketbase_csharp_sdk.Sse;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -16,10 +17,12 @@ namespace pocketbase_csharp_sdk.Services
     public abstract class BaseSubCrudService : BaseService
     {
         private readonly PocketBase client;
+        private readonly SseClient sse;
 
         public BaseSubCrudService(PocketBase client)
         {
             this.client = client;
+            this.sse = new SseClient(client);
         }
 
         public async Task<PagedCollectionModel<T>> ListAsync<T>(
@@ -108,20 +111,33 @@ namespace pocketbase_csharp_sdk.Services
 
             try
             {
-                var url = client.BuildUrl("/api/realtime").ToString();
-                var responseStream = await client._httpClient.GetStreamAsync(url);
+                sse.ConnectAsync(callback);
 
-                var buffer = new byte[4096];
-                while (true)
-                {
-                    var readCount = await responseStream.ReadAsync(buffer, 0, buffer.Length);
-                    if (readCount > 0)
-                    {
-                        var data = Encoding.UTF8.GetString(buffer, 0, readCount);
+                while(!sse.IsConnected)
+                    await Task.Delay(500);
 
-                    }
-                    await Task.Delay(125);
-                }
+
+                Dictionary<string, object> body = new();
+                body.Add("clientId", sse.Id);
+                body.Add("subscriptions", new List<string> { "tags" });
+
+                var url = $"api/realtime";
+                await client.SendAsync(url, HttpMethod.Post, body: body);
+
+                //var url = client.BuildUrl("/api/realtime").ToString();
+                //var responseStream = await client._httpClient.GetStreamAsync(url);
+
+                //var buffer = new byte[4096];
+                //while (true)
+                //{
+                //    var readCount = await responseStream.ReadAsync(buffer, 0, buffer.Length);
+                //    if (readCount > 0)
+                //    {
+                //        var data = Encoding.UTF8.GetString(buffer, 0, readCount);
+
+                //    }
+                //    await Task.Delay(125);
+                //}
 
                 //using StreamReader reader = new StreamReader(response);
                 //SseMessage message = new SseMessage();
@@ -143,41 +159,6 @@ namespace pocketbase_csharp_sdk.Services
             }
         }
 
-        private bool ProcessMessage(string? line, SseMessage message)
-        {
-            Regex regex = new Regex("^(\\w+)[\\s\\:]+(.*)?$");
-            if (string.IsNullOrWhiteSpace(line))
-            {
-                return true;
-            }
-
-            var match = regex.Match(line);
-            if (match is null)
-            {
-                return false;
-            }
-
-            var field = match.Groups[1].Value ?? "";
-            var value = match.Groups[2].Value ?? "";
-
-            switch (field)
-            {
-                case "id":
-                    message.Id = value;
-                    break;
-                case "event":
-                    message.Event = value;
-                    break;
-                case "retry":
-                    message.Retry = SafeConvert.ToInt(value, 0);
-                    break;
-                case "data":
-                    message.Data = value;
-                    break;
-            }
-
-            return false;
-        }
 
         public async Task UploadFileAsync(string sub, string field, string fileName, Stream stream)
         {
