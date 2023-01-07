b283b9158f3d48f67e6d1e1ab820646741f5d885
iluvadev <iluvadev@gmail.com>
2023-01-07 09:35:16 +0100

    Update SseClient.cs

--- pocketbase-csharp-sdk/Sse/SseClient.cs
+++ pocketbase-csharp-sdk/Sse/SseClient.cs
@@ -68,7 +68,7 @@ namespace pocketbase_csharp_sdk.Sse
                 if (!isTextEventStream)
                     throw new InvalidOperationException("Invalid resource content type");
 
-                var stream = await response.Content.ReadAsStreamAsync();
+                var stream = await response.Content.ReadAsStreamAsync(token);
                 var buffer = new byte[4096];
                 while (!token.IsCancellationRequested)
                 {
