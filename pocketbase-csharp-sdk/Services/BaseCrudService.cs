3cb2edbc64b3d463a127fa0d4b191511f2231399
PRCV1 <lukas.mueller.01@web.de>
2023-01-08 21:41:25 +0100

    documentation for logservice and basecrudservice

--- pocketbase-csharp-sdk/Services/BaseCrudService.cs
+++ pocketbase-csharp-sdk/Services/BaseCrudService.cs
@@ -19,6 +19,16 @@ namespace pocketbase_csharp_sdk.Services
             this.client = client;
         }
 
+        /// <summary>
+        /// retrieves a paginated list of objects of type T
+        /// </summary>
+        /// <param name="page">The page number of the list to retrieve. Default is 1</param>
+        /// <param name="perPage">The number of objects per page. Default is 30.</param>
+        /// <param name="filter">A filter string to apply to the list. Default is null.</param>
+        /// <param name="sort">A sort string to apply to the list. Default is null.</param>
+        /// <param name="cancellationToken">A cancellation token to cancel the operation. Default is the default cancellation token</param>
+        /// <returns>A PagedCollectionModel<T> object containing the paginated list of objects.</returns>
+        /// <exception cref="ClientException"></exception>
         public virtual async Task<PagedCollectionModel<T>> ListAsync(int page = 1, int perPage = 30, string? filter = null, string? sort = null, CancellationToken cancellationToken = default)
         {
             var query = new Dictionary<string, object?>()
@@ -38,6 +48,14 @@ namespace pocketbase_csharp_sdk.Services
             return response;
         }
 
+        /// <summary>
+        /// retrieves a full list of objects of type T, by making multiple calls to the `ListAsync` function and paginating through the results.
+        /// </summary>
+        /// <param name="batch">The number of objects to retrieve per call to `ListAsync`. Default is 100.</param>
+        /// <param name="filter">A filter string to apply to the list. Default is null.</param>
+        /// <param name="sort">A sort string to apply to the list. Default is null.</param>
+        /// <param name="cancellationToken">A cancellation token to cancel the operation. Default is the default cancellation token.</param>
+        /// <returns>An IEnumerable<T> object containing the full list of objects.</returns>
         public virtual async Task<IEnumerable<T>> GetFullListAsync(int batch = 100, string? filter = null, string? sort = null, CancellationToken cancellationToken = default)
         {
             List<T> result = new();
--- pocketbase-csharp-sdk/Services/LogService.cs
+++ pocketbase-csharp-sdk/Services/LogService.cs
@@ -21,13 +21,34 @@ namespace pocketbase_csharp_sdk.Services
             this.client = client;
         }
 
+        /// <summary>
+        /// retrieves a specific request log from the API by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the request log to retrieve.</param>
+        /// <param name="body">The request body to send to the API. Default is null.</param>
+        /// <param name="query">The query parameters to send to the API. Default is null.</param>
+        /// <param name="headers">The headers to send to the API. Default is null.</param>
+        /// <param name="cancellationToken">A cancellation token to cancel the operation. Default is the default cancellation token.</param>
+        /// <returns>A LogRequestModel object containing the request log.</returns>
         public async Task<LogRequestModel?> GetRequestAsync(string id, IDictionary<string, object>? body = null, IDictionary<string, object?>? query = null, IDictionary<string, string>? headers = null, CancellationToken cancellationToken = default)
         {
-            var url = $"{BasePath()}/{HttpUtility.HtmlEncode(id)}";
+            var url = $"{BasePath()}/{UrlEncode(id)}";
             var response = await client.SendAsync<LogRequestModel>(url, HttpMethod.Get, headers: headers, query: query, body: body, cancellationToken: cancellationToken);
             return response;
         }
 
+        /// <summary>
+        /// retrieves a paginated list of request logs from the API.
+        /// </summary>
+        /// <param name="page">The page number of the list to retrieve. Default is 1.</param>
+        /// <param name="perPage">The number of request logs per page. Default is 30.</param>
+        /// <param name="filter">A filter string to apply to the list. Default is null.</param>
+        /// <param name="sort">A sort string to apply to the list. Default is null.</param>
+        /// <param name="body">The request body to send to the API. Default is null.</param>
+        /// <param name="query">The query parameters to send to the API. Default is null.</param>
+        /// <param name="headers">The headers to send to the API. Default is null.</param>
+        /// <param name="cancellationToken">A cancellation token to cancel the operation. Default is the default cancellation token.</param>
+        /// <returns>A ResultList<LogRequestModel> object containing the paginated list of request logs.</returns>
         public async Task<ResultList<LogRequestModel>?> GetRequestsAsync(int page = 1, int perPage = 30, string? filter = null, string? sort = null, IDictionary<string, object>? body = null, IDictionary<string, object?>? query = null, IDictionary<string, string>? headers = null, CancellationToken cancellationToken = default)
         {
             query ??= new Dictionary<string, object?>();
@@ -41,6 +62,13 @@ namespace pocketbase_csharp_sdk.Services
             return response;
         }
 
+        /// <summary>
+        /// retrieves statistics for the request logs
+        /// </summary>
+        /// <param name="query">The query parameters to send to the API. Default is null.</param>
+        /// <param name="headers">The headers to send to the API. Default is null.</param>
+        /// <param name="cancellationToken">A cancellation token to cancel the operation. Default is the default cancellation token.</param>
+        /// <returns>An IEnumerable<LogRequestStatistic> object containing the request log statistics.</returns>
         public async Task<IEnumerable<LogRequestStatistic>?> GetRequestsStatisticsAsync(IDictionary<string, object?>? query = null, IDictionary<string, string>? headers = null, CancellationToken cancellationToken = default)
         {
             var url = $"{BasePath()}/stats";
