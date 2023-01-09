fa30c2026ebf1813ab06ee723a295de971c37e52
PRCV1 <lukas.mueller.01@web.de>
2023-01-09 21:30:42 +0100

    documentation for BaseAuthService

--- pocketbase-csharp-sdk/Services/BaseAuthService.cs
+++ pocketbase-csharp-sdk/Services/BaseAuthService.cs
@@ -11,6 +11,16 @@ namespace pocketbase_csharp_sdk.Services
             this.client = client;
         }
 
+        /// <summary>
+        /// authenticates a user with the API using their email and password.
+        /// </summary>
+        /// <param name="email">The email of the user to authenticate.</param>
+        /// <param name="password">The password of the user to authenticate.</param>
+        /// <param name="body">The request body to send to the API. Default is null.</param>
+        /// <param name="query">The query parameters to send to the API. Default is null.</param>
+        /// <param name="headers">The headers to send to the API. Default is null.</param>
+        /// <param name="cancellationToken">A cancellation token to cancel the operation. Default is the default cancellation token.</param>
+        /// <returns>An object of type T containing the authenticated user's information.</returns>
         public async Task<T?> AuthenticateWithPasswordAsync(string email, string password, IDictionary<string, object>? body = null, IDictionary<string, object?>? query = null, IDictionary<string, string>? headers = null, CancellationToken cancellationToken = default)
         {
             body ??= new Dictionary<string, object>();
@@ -25,6 +35,14 @@ namespace pocketbase_csharp_sdk.Services
             return result;
         }
 
+        /// <summary>
+        /// refreshes an authenticated user's token
+        /// </summary>
+        /// <param name="body">The request body to send to the API. Default is null.</param>
+        /// <param name="query">The query parameters to send to the API. Default is null.</param>
+        /// <param name="headers">The headers to send to the API. Default is null.</param>
+        /// <param name="cancellationToken">A cancellation token to cancel the operation. Default is the default cancellation token.</param>
+        /// <returns>An object of type T containing the authenticated user's updated information.</returns>
         public async Task<T?> RefreshAsync(IDictionary<string, object>? body = null, IDictionary<string, object?>? query = null, IDictionary<string, string>? headers = null, CancellationToken cancellationToken = default)
         {
             var url = $"{BasePath()}/auth-refresh";
@@ -35,15 +53,34 @@ namespace pocketbase_csharp_sdk.Services
             return result;
         }
 
-        public async Task RequestPasswordResetAsync(string email, IDictionary<string, object>? body = null, IDictionary<string, object?>? query = null, IDictionary<string, string>? headers = null, CancellationToken cancellationToken = default)
+        /// <summary>
+        /// sends a password reset request for a specific email.
+        /// </summary>
+        /// <param name="email">The email of the user to send the password reset request to.</param>
+        /// <param name="body">The request body to send to the API. Default is null.</param>
+        /// <param name="query">The query parameters to send to the API. Default is null.</param>
+        /// <param name="headers">The headers to send to the API. Default is null.</param>
+        /// <param name="cancellationToken">A cancellation token to cancel the operation. Default is the default cancellation token.</param>
+        public Task RequestPasswordResetAsync(string email, IDictionary<string, object>? body = null, IDictionary<string, object?>? query = null, IDictionary<string, string>? headers = null, CancellationToken cancellationToken = default)
         {
             body ??= new Dictionary<string, object>();
             body.Add("email", email);
 
             var url = $"{BasePath()}/request-password-reset";
-            await client.SendAsync(url, HttpMethod.Post, headers: headers, query: query, body: body, cancellationToken: cancellationToken);
+            return client.SendAsync(url, HttpMethod.Post, headers: headers, query: query, body: body, cancellationToken: cancellationToken);
         }
 
+        /// <summary>
+        /// confirms a password reset request using a reset token, and a new password.
+        /// </summary>
+        /// <param name="passwordResetToken">The password reset token sent to the user's email.</param>
+        /// <param name="password">The new password to set for the user.</param>
+        /// <param name="passwordConfirm">The confirmation of the new password.</param>
+        /// <param name="body">The request body to send to the API. Default is null.</param>
+        /// <param name="query">The query parameters to send to the API. Default is null.</param>
+        /// <param name="headers">The headers to send to the API. Default is null.</param>
+        /// <param name="cancellationToken">A cancellation token to cancel the operation. Default is the default cancellation token.</param>
+        /// <returns>An object of type T containing the authenticated user's updated information.</returns>
         public async Task<T?> ConfirmPasswordResetAsync(string passwordResetToken, string password, string passwordConfirm, IDictionary<string, object>? body = null, IDictionary<string, object?>? query = null, IDictionary<string, string>? headers = null, CancellationToken cancellationToken = default)
         {
             body ??= new Dictionary<string, object>();
